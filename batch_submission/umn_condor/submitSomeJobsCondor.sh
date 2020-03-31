7f2e4fe15e360bbf5dbf65f62cec6ea969ca8a3a
Anthony Villano <anthony.villano@ucdenver.edu>
2020-03-31 11:00:09 -0500

    [ANV] first commit, not working code

---
+++ batch_submission/umn_condor/submitSomeJobsCondor.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# use this one for standard beryllium
+#for i in $(seq 1 55) 
+#do
+#  for j in 1 2 3 4 5  
+#  do
+#    ./condorNeutReflect -n 1 -set 18 -src 88Y -d -6 -dr ${i} -dt ${j} -bepure -ngen 1000000 -exe
+#  done
+#done
+
+# use this one for standard beryllium
+#for i in $(seq 1 55) 
+#do
+#  for j in 1.17	2.34 3.51 4.69  
+#  do
+#    ./bsubNeutReflect -n 1 -set 13 -src 124Sb -d -3 -dr ${i} -dt ${j} -bepure -ngen 1000000 -exe
+#  done
+#done
+
+# use this one for BeO 
+#for i in $(seq 1 55) 
+#do
+#  for j in 2 4 6 8 
+#  do
+#    ./bsubNeutReflect -n 1 -set 13 -src 124Sb -d -3 -dr ${i} -dt ${j} -ngen 1000000 -exe
+#  done
+#done
+
+#use this for ybe 0x0004 photoneutron jobs
+#./condorPhotoNSuperSim -n 100 -src ybe -set 4 -ngen 1000000 -prim -otype root -merge -template soudan_photon_neutron_bridgeshiftUpdate.mac.template -exe
+
+#use this for sbbe 0x0004 photoneutron jobs
+#./condorPhotoNSuperSim -n 100 -src sbbe -set 4 -ngen 1000000 -prim -otype root -merge -template soudan_photon_neutron_bridgeshiftUpdate.mac.template -exe
+
+#use this for ybe 0x0005 photoneutron jobs
+#./condorPhotoNSuperSim -n 50 -src ybe -set 5 -ngen 1000000 -prim -otype root -merge -template soudan_photon_neutron_bridgeshiftUpdate.mac.template -exe
+
+#use this for sbbe 0x0005 photoneutron jobs
+#./condorPhotoNSuperSim -n 50 -src sbbe -set 5 -ngen 1000000 -prim -otype root -merge -template soudan_photon_neutron_bridgeshiftUpdate.mac.template -exe
+
+#use this for ybe 0x0006 photoneutron jobs
+./condorPhotoNSuperSim -n 140 -src ybe -set 6 -ngen 1000000 -prim -otype root -merge -template soudan_photon_neutron_bridgeshiftUpdate.mac.template -exe
+
+#use this for sbbe 0x0006 photoneutron jobs
+#./condorPhotoNSuperSim -n 140 -src sbbe -set 6 -ngen 1000000 -prim -otype root -merge -template soudan_photon_neutron_bridgeshiftUpdate.mac.template -exe
